06bed91db2a8ac0461b9976f9fa35339c559c1dc
makoshika <buryc405@bucs.dawson.edu>
2020-04-24 12:09:51 -0600

    first commit

---
+++ src/components/app.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+
+export default class App extends Component {
+  render() {
+    return (
+      <div className='app'>
+        <h1>Mad Libs</h1>
+        <h2>BIG and small</h2>
+      </div>
+    );
+  }
+}
